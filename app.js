74c414749dd073cd011da3bdfe4f62afb7e8c963
juyonglee <juyonglee0208@gmail.com>
2020-03-27 18:24:56 +0900

    Passport 인증 기능 사용을 위한 초기 설정

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// Passport 사용을 위한 설정
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,9 +21,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Login 기능 Test를 위한 User 추가
+const testUser = {
+  "id": "juyonglee0208",
+  "name": "Juyong Lee",
+  "password": "0000"
+};
+
+// 인증을 위한 Strategies 정의
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      if(testUser.id != username || testUser.password != password) {
+        done(null, false)
+      }
+      return done(null, testUser);
+    }
+));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
