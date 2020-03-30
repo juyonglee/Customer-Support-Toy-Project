fd24a04cb3b4efda2be2fdfeba55845cfb2e135d
juyonglee <juyonglee0208@gmail.com>
2020-03-30 16:53:37 +0900

    OTP 기능을 적용한 Session 동작 절차 정의

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.user){
+  if(req.user && req.session.otp){
     res.render('index', { title: req.user.name });
+  } else if(req.user) {
+    res.render('otp')
   } else {
     res.redirect("auth/login")
   }
